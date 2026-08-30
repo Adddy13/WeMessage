d1675747203553c8d64dacc190dd74fb50214708
Adddy13 <aditiwari765@gmail.com>
2026-08-31 00:45:36 +0530

    Remove job.start() call from app.listen in production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,4 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
   connectDB();
   console.log("Server is running on port:", PORT);
-
-
-   if (process.env.NODE_ENV === "production") job.start();
 });
